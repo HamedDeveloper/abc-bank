d84043be69ae19749d044b4d357db87557c4ad3c
Ed Mogadam <hamed.salimi@gmail.com>
2016-05-07 20:13:28 -0400

    Added tests

--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -1,15 +1,37 @@
 package com.abc;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
     private static final double DOUBLE_DELTA = 1e-15;
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void deposit(){
+    public void deposit() {
         Account checkingAccount = new Account(Account.CHECKING, 500);
         assertEquals(500, checkingAccount.getBalance(), DOUBLE_DELTA);
+        checkingAccount.deposit(300);
+        assertEquals(800, checkingAccount.getBalance(), DOUBLE_DELTA);
     }
+
+    @Test
+    public void withdraw() {
+        Account checkingAccount = new Account(Account.CHECKING, 500);
+        checkingAccount.withdraw(200);
+        assertEquals(300, checkingAccount.getBalance(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void withdrawMoreThanBalance() {
+        thrown.expect(IllegalArgumentException.class);
+        Account checkingAccount = new Account(Account.CHECKING, 500);
+        checkingAccount.withdraw(700);
+    }
+
 }
