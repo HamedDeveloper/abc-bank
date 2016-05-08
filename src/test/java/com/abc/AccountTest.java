ef1b99f2e9100e6693c4ae3aa1f376f4a0b629cd
Ed Mogadam <hamed.salimi@gmail.com>
2016-05-07 20:08:39 -0400

    added deposit test

--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -2,15 +2,14 @@ package com.abc;
 
 import org.junit.Test;
 
-import java.math.BigDecimal;
-
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
+    private static final double DOUBLE_DELTA = 1e-15;
 
     @Test
     public void deposit(){
-        Account checkingAccount = new Account(Account.CHECKING, BigDecimal.valueOf(500));
-        assertEquals(BigDecimal.valueOf(500), checkingAccount.getBalance());
+        Account checkingAccount = new Account(Account.CHECKING, 500);
+        assertEquals(500, checkingAccount.getBalance(), DOUBLE_DELTA);
     }
 }
